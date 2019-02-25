32a1e8ced740665468e92eec2ac7bf5dca31f25e
Darius Foo <darius.foo.tw@gmail.com>
2019-02-25 08:29:14 +0800

    Add stable topological ordering

--- main.js
+++ main.js
@@ -4,22 +4,65 @@ var sql = window.SQL;
 function todo() {
   var db = new sql.Database();
   // type has to be integer, not int, if null is used to get an unused id without autoincrement
+
+  // sadly, even though we want
+  // unique (ordinal, project_id)
+  // on the tasks table, we leave it out for practical reasons.
+  // e.g. two rows contain 1 and 2, and i run update table set col = col + 1;
+  // the order in which the rows are updated matters, so one order is fine
+  // and the other produces a unique constraint violation. there doesn't
+  // seem to be a nice way to handle this without hacks involving
+  // copying the rows into a temporary table.
+
   db.run(`
+  pragma foreign_keys = on;
+
   create table tasks (
     id integer primary key,
     done boolean,
-    desc text);
+    desc text,
+    project_id integer,
+    ordinal integer,
+    foreign key (project_id) references projects(id)
+  );
+  create table projects (
+    id integer primary key,
+    name text,
+    ordinal integer,
+    unique (ordinal)
+  );
+  create table task_deps (
+    t_from integer,
+    t_to integer,
+    foreign key (t_from) references tasks(id) on delete cascade,
+    foreign key (t_to) references tasks(id) on delete cascade,
+    unique (t_from, t_to)
+  );
   create table tags (
     task_id int,
     name text,
-    foreign key (task_id) references tasks(id));
+    foreign key (task_id) references tasks(id) on delete cascade
+  );
+  create table queries (
+    query text
+  );
   `);
 
   db.run(`
-    insert into tasks values (1, 0, 'hello');
-    insert into tags values (1, 'inbox');
-    insert into tags values (1, 'next');`);
+    insert into projects values (null, 'inbox', 0);
+    insert into tasks values (null, 0, 'hello', 1, 0);
+    insert into tags values (1, 'home');
+    insert into tags values (1, 'commute');
+  `);
   return db;
+}
+
+function results(r) {
+  if (!r.length) {
+    return [];
+  } else {
+    return r[0].values;
+  }
 }
 
 var { patch, elementVoid, elementClose, elementOpen, text } = IncrementalDOM;
@@ -29,8 +72,22 @@ var database = todo();
 // derived/transient state which is okay to forget
 var state = {
   more: false,
-  chosenTag: 'inbox',
+  chosenProject: { name: 'inbox', id: 1 },
+  blockee: null,
+  dirty: false,
 };
+
+// idempotent, pre-update initialization. occurs after a database is loaded,
+// so should restore the starting state
+function on_init() {
+  database.run('pragma foreign_keys = on;');
+
+  let [id, name] = results(database.exec('select id, name from projects order by ordinal limit 1'))[0];
+  state.chosenProject = { name, id };
+
+  state.blockee = null;
+  state.dirty = false;
+}
 
 function render() {
 
@@ -40,26 +97,37 @@ function render() {
       var text = e.target.value;
       // the null uses the existing sequence, however the create event loses its id
       database.exec(`
-        insert into tasks values (null, 0, '${text}');
-        insert into tags select last_insert_rowid(), '${window.state.chosenTag}';
+        begin;
+        update tasks set ordinal = ordinal + 1 where project_id = ${window.state.chosenProject.id};
+        insert into tasks values (null, 0, '${text}', '${window.state.chosenProject.id}', 0);
+        commit;
       `);
       e.target.value = '';
+      window.state.dirty = true;
       update();
     }
   }]);
 
   elementOpen('select', null, ['onchange', e => {
-    window.state.chosenTag = e.target.value;
+    let id = e.target.options[e.target.selectedIndex].getAttribute('project_id')
+    window.state.chosenProject = { name: e.target.value, id: id };
     update();
   }]);
   // this should not be empty
-  var tags = database.exec('select distinct name from tags;')[0].values;
-  tags.forEach(t => {
-    elementOpen('option', null, ['value', t]);
-    text(t)
+  var projects = results(database.exec('select id, name from projects order by ordinal;'));
+  projects.forEach(t => {
+    let [id, name] = t;
+    elementOpen('option', null, ['value', name, 'project_id', id]);
+    text(name)
     elementClose('option');
   });
   elementClose('select');
+
+  if (window.state.dirty) {
+    elementOpen('span');
+    text('*')
+    elementClose('span');
+  }
 
   elementOpen('div');
   elementOpen('a', null, ['href', '#', 'onclick', _ => {
@@ -75,9 +143,13 @@ function render() {
     elementOpen('a', null, ['href', '#', 'onclick', e => {
       var result = [];
       result.push('tasks')
-      database.exec('select * from tasks;')[0].values.forEach(v => result.push(JSON.stringify(v)));
+      results(database.exec('select * from tasks;')).forEach(v => result.push(JSON.stringify(v)));
+      result.push('projects')
+      results(database.exec('select * from projects;')).forEach(v => result.push(JSON.stringify(v)));
+      result.push('deps')
+      results(database.exec('select * from task_deps;')).forEach(v => result.push(JSON.stringify(v)));
       result.push('tags')
-      database.exec('select * from tags;')[0].values.forEach(v => result.push(JSON.stringify(v)));
+      results(database.exec('select * from tags;')).forEach(v => result.push(JSON.stringify(v)));
 
       var exportField = document.createElement('textarea');
       exportField.value = result.join('\n');
@@ -90,7 +162,8 @@ function render() {
 
     elementOpen('div');
     elementOpen('a', null, ['href', '#', 'onclick', _ => {
-      database.run(`delete from tasks where id in (select t.id from tasks t inner join tags ta on ta.task_id = t.id where t.done and ta.name = '${window.state.chosenTag}');`);
+      window.database.run(`delete from tasks where id in (select t.id from tasks t inner join projects p on p.id = t.project_id where t.done and p.id = '${window.state.chosenProject.id}');`);
+      window.state.dirty = true;
       update();
     }]);
     text('clear done')
@@ -120,6 +193,8 @@ function render() {
         .filesUpload({contents: database.export().buffer, path: '/todo.db', mode: {'.tag': 'overwrite'}})
         .then(function() {
           console.log('saved');
+          window.state.dirty = false;
+          update();
         })
         .catch(function (error) {
           console.log(error);
@@ -146,6 +221,7 @@ function render() {
             var ab = e.target.result;
             var u8a  = new Uint8Array(ab);
             window.database = new sql.Database(u8a);
+            on_init();
             update();
           });
           reader.readAsArrayBuffer(blob);
@@ -163,35 +239,57 @@ function render() {
   elementOpen('ul');
 
   var tasks = database.exec(`
-    select distinct t.*
+    select t.*
     from tasks t
-    left join tags ta on ta.task_id = t.id
-    where ta.name = '${window.state.chosenTag}';`);
+    where t.project_id = '${window.state.chosenProject.id}'
+    order by t.done, t.ordinal;`);
 
-  // TODO factor out
-  if (!tasks.length) {
-    tasks = [];
-  } else {
-    tasks = tasks[0].values;
+  // assume that dependencies don't span projects
+  var deps = database.exec(`
+    select distinct td.*
+    from task_deps td, tasks t
+    where t.project_id = '${window.state.chosenProject.id}'
+    and td.t_from = t.id or td.t_to = t.id;`);
+
+  tasks = results(tasks);
+  deps = results(deps);
+
+  function reorder_tasks() {
+    let done = tasks.filter(t => t[1]);
+    let undone = tasks.filter(t => !t[1]);
+
+    let task_map = {};
+    undone.forEach(u => task_map[u[0]] = u);
+
+    let ids = undone.map(t => t[0]);
+
+    let ids1 = stable_toposort(deps, ids);
+
+    return ids1.map(i => task_map[i]).concat(done);
   }
 
+  tasks = reorder_tasks();
+
   tasks.forEach(t => {
-    let [id, checked, desc] = t;
+    let [id, checked, desc, project_id, ordinal] = t;
     checked = !!checked;
     elementOpen('li');
 
     function on_change(id, checked) {
-      database.run(`update tasks set done = ${+checked} where id = ${id}`);
+      window.database.run(`update tasks set done = ${+checked} where id = ${id}`);
+      window.state.dirty = true;
       update();
     }
 
     // work around https://github.com/google/incremental-dom/issues/198
     elementVoid.apply(null,
-      ['input', id, ['type', 'checkbox', 'onchange', e => on_change(id, e.target.checked)]]
+      ['input', `task-checkbox-${id}`, ['type', 'checkbox', 'onchange', e => on_change(id, e.target.checked)]]
         .concat(checked ? ['checked', 'lol'] : []))
     text(desc);
 
-    elementOpen('a', null, ['href', '#', 'onclick', e => {
+    elementOpen('a', `task-more-${id}`, ['href', '#', 'onclick', e => {
+      e.target.insertAdjacentElement("beforeBegin", document.createElement('br'));
+
       var tagsField = document.createElement('input');
       tagsField.setAttribute("placeholder", "tags");
       var tags = database.exec(`select name from tags where task_id = ${id}`);
@@ -202,18 +300,71 @@ function render() {
         if (e.keyCode === 13) {
           var text = e.target.value;
           text.split(',').forEach(t => {
-            database.exec(`
+            window.database.exec(`
               delete from tags where task_id = ${id};
               insert into tags values (${id}, '${t.trim()}')
             `);
           });
+          window.state.dirty = true;
           update();
         }
       };
-      e.target.insertAdjacentElement("afterend", tagsField);
+      e.target.insertAdjacentElement("beforeBegin", tagsField);
+
+      e.target.insertAdjacentElement("beforeBegin", document.createElement('br'));
+
+      var blockedBy = document.createElement('a');
+      blockedBy.innerHTML = 'blocked by';
+      blockedBy.setAttribute('href', '#');
+      blockedBy.onclick = e => {
+        window.state.blockee = id;
+      };
+      e.target.insertAdjacentElement("beforeBegin", blockedBy);
+      e.target.insertAdjacentElement("beforeBegin", document.createElement('br'));
+
+      var byThis = document.createElement('a');
+      byThis.setAttribute('href', '#');
+      byThis.innerHTML = 'this';
+      byThis.onclick = e => {
+        if (window.state.blockee !== null && window.state.blockee !== id) {
+          window.database.exec(`insert into task_deps values (${window.state.blockee}, ${id})`);
+          window.state.blockee = null;
+          window.state.dirty = true;
+          update();
+        }
+      };
+      e.target.insertAdjacentElement("beforeBegin", byThis);
+      e.target.insertAdjacentElement("beforeBegin", document.createElement('br'));
+
+      var toTop = document.createElement('a');
+      toTop.setAttribute('href', '#');
+      toTop.innerHTML = 'top';
+      toTop.onclick = e => {
+        window.database.exec(`
+          begin;
+          update tasks set ordinal = ordinal + 1 where project_id = ${window.state.chosenProject.id} and id <> ${id};
+          update tasks set ordinal = 0 where id = ${id};
+          commit;
+        `);
+        window.state.dirty = true;
+        update();
+      };
+      e.target.insertAdjacentElement("beforeBegin", toTop);
+      e.target.insertAdjacentElement("beforeBegin", document.createElement('br'));
+
+      var toBottom = document.createElement('a');
+      toBottom.setAttribute('href', '#');
+      toBottom.innerHTML = 'bottom';
+      toBottom.onclick = e => {
+        window.database.exec(`update tasks set ordinal = 1+(select max(ordinal) from tasks where project_id = ${window.state.chosenProject.id}) where id = ${id};`);
+        window.state.dirty = true;
+        update();
+      };
+      e.target.insertAdjacentElement("beforeBegin", toBottom);
+
       e.target.remove();
     }]);
-    text('?')
+    text('...')
     elementClose('a');
 
     elementClose('li');
---
+++ stable_toposort.js
@@ -0,0 +1,92 @@
+(function () {
+
+  // converts an edge list into a dependency map
+  function to_dep_map(edges) {
+    var deps = {}
+    edges.forEach(e => {
+      let [a, b] = e;
+      deps[a] = deps[a] || {};
+      deps[a][b] = deps[a][b] || true;
+    });
+    return deps;
+  }
+
+  // traverses a dependency map to collect all transitive dependencies
+  function all_transitive(map, a) {
+    let neighbours = Object.keys(map[a] || {});
+    if (neighbours.length === 0) {
+      return [];
+    } else {
+      let children = neighbours.map(n => {
+        return all_transitive(map, n);
+      }).reduceRight((t, c) => t.concat(c));
+      return neighbours.concat(children);
+    }
+  }
+
+  // null-safe direct dependency check
+  function has_direct_dep(map, a, b) {
+    if (!map[a]) {
+      map[a] = {};
+    }
+    return map[a][b];
+  }
+
+  // memoizing transitive dependency check
+  // TODO does not memoize as much as it should,
+  // e.g. does not do b -> c when doing a -> b -> c
+  function has_transitive_dep(map, a, b) {
+    let r = has_direct_dep(map, a, b);
+    if (r === undefined) {
+      // not direct, maybe transitive
+      all_transitive(map, a).forEach(c => {
+        map[a][c] = true;
+      });
+      if (!map[a][b]) {
+        map[a][b] = false;
+      }
+      return map[a][b];
+    } else {
+      // direct or transitive
+      return r;
+    }
+  }
+
+  // http://blog.gapotchenko.com/stable-topological-sort
+  function stable_toposort(edges, list) {
+
+    // edges is a list of [a, b] pairs where a -- depends on -> b
+    let direct = to_dep_map(edges);
+    let transitive = to_dep_map(edges);
+
+    let result = list.slice()
+    let n = result.length;
+    while (true) {
+      for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < i; ++j) {
+          if (has_direct_dep(direct, result[j], result[i])) {
+            let jOnI = has_transitive_dep(transitive, result[j], result[i]);
+            let iOnJ = has_transitive_dep(transitive, result[i], result[j]);
+
+            let circularDependency = jOnI && iOnJ;
+
+            if (!circularDependency) {
+              let t = result[i];
+              result.splice(i, 1);
+              result.splice(j, 0, t);
+              continue;
+            }
+          }
+        }
+      }
+      break;
+    }
+
+    return result;
+  }
+
+  // stable_toposort([[1,2],[2,4]], [1,2,3,4,5,6])
+
+  window.stable_toposort = stable_toposort;
+
+})();
